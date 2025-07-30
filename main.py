a3363af917c810e488ccd0863af6f3f3ef811201
Serhii  Tustanovskyi <j264@pjwstk.edu.pl>
2025-07-30 16:45:36 +0200

    feat: save to file

--- main.py
+++ main.py
@@ -1,23 +1,70 @@
-TODOS = []
-STATUS = []
+import os
+import random
+import time
 
-def addTodo():
+
+FILE = "todo.txt"
+
+current_dir = os.path.dirname(__file__)
+FILE_PATH = os.path.join(current_dir, FILE)
+
+if not os.path.exists(FILE_PATH):
+    try:
+        with open(FILE_PATH, "w", encoding="utf-8") as f:
+            f.write(" ")
+    except:
+        print("Failed to create file.")        
+
+def parseBool(s):
+    return s.strip().lower() == "true"
+
+def save(todos, statuses):
+    try:
+        with open(FILE_PATH, "w", encoding="utf-8") as file:
+            for (id, title, priority), status in zip(todos, statuses):
+                file.write(f"{id}|{title}|{priority}|{status}\n")
+    except:
+        print("Failed to save file.")
+    else:
+        print("File is saved")
+
+def load():
+    todos = []
+    statuses = []
+    try:
+        with open(FILE_PATH, "r", encoding="utf-8") as file:
+            for line in file:
+                id, title, priority, status = line.strip().split("|")
+                todos.append(((int(id)), title, priority))
+                statuses.append(parseBool(status))
+        return todos, statuses
+    except  Exception as e:
+        print(f"Failed to load file: {e}")
+        return [], []
+    
+
+def addTodo(todos, statuses):
     title = input("Input title: ")
     priority = input("Input priority: ")
     id = 0
 
-    if len(TODOS) > 0:
-        id = len(TODOS)
+    if len(todos) > 0:
+        id = len(todos)
 
-    TODOS.append((id, title, priority))
-    STATUS.append(False)
+    todos.append((id, title, priority))
+    statuses.append(False)
+    save(todos, statuses)
     
 
 
-def removeTodo():
+def removeTodo(todos, statuses):
     indexToRemove = int(input("Input index to remove: "))
-    del TODOS[indexToRemove]
-    del STATUS[indexToRemove]
+    try:
+        del todos[indexToRemove]
+        del statuses[indexToRemove]
+        save(todos, statuses)
+    except IndexError as e:
+        print(f"Failed to remove todo: {e}")         
 
 def getStatus(status):
     if status:
@@ -25,13 +72,20 @@ def getStatus(status):
     else:
        return  "[ ]"    
 
-def showTodos():
-    for index ,(id, title, priority) in enumerate(TODOS):
-        print(f"id: {id}, title: {title}, priority: {priority}, status: {getStatus(STATUS[index])}")
+def showTodos(todos, statuses):
+    if len(todos) > 0:
+        for index ,(id, title, priority) in enumerate(todos):
+            print(f"id: {id}, title: {title}, priority: {priority}, status: {getStatus(statuses[index])}")
+    else:
+        print("Empty todos")
 
-def changeStatus(done):
+def changeStatus(todos, statuses, done):
     indexToChangeStatus = int(input("Input index of todos which need to change status: "))
-    STATUS[indexToChangeStatus]  = done  
+    try:
+        statuses[indexToChangeStatus]  = done
+        save(todos, statuses)
+    except IndexError as e:
+        print(f"Failed to change status: {e}")      
 
 def main():
     while True:
@@ -43,18 +97,21 @@ def main():
         print("4 - change status to done: ")
         print("5 - change status to not done: ")
         print("--------------------------------------")
+
+        todos, statuses = load()
+
         userInput = input("Input option: ")
         match userInput:
             case "1":
-                addTodo()
+                addTodo(todos, statuses)
             case "2":
-                showTodos()
+                showTodos(todos, statuses)
             case "3":
-                removeTodo()
+                removeTodo(todos, statuses)
             case "4":
-                changeStatus(True)
+                changeStatus(todos, statuses, True)
             case "5":
-                changeStatus(False)    
+                changeStatus(todos, statuses, False)    
             case "0":
                 break
             case _:
